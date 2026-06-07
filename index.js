1018bc3d97afcc00fc9799610411a477f8077472
santra <santrapkoshy98@gmail.com>
2026-06-07 18:05:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
