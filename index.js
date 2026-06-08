ac4e7582dc09c515f088ebdf9b57a18dffc2de8d
santra <santrapkoshy98@gmail.com>
2026-06-08 16:07:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   return res.json({
     message: "Checking Changes",
-    status: "Github actions",
+    status: "Github actions latest",
   });
 });
 
